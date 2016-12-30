7bc382cf7c191dd2e6295120db4cb906882e43b8
Wolfgang Rosner <wrosner@tirnet.de>
2016-12-30 22:01:13 +0100

    add 'lo_batt' field to 'raw' table

--- raw.sql
+++ raw.sql
@@ -1,33 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 2.6.3-pl1
+-- version 3.4.11.1deb2+deb7u7
 -- http://www.phpmyadmin.net
--- 
+--
 -- Host: localhost
--- Erstellungszeit: 23. Februar 2014 um 23:07
--- Server Version: 4.1.13
--- PHP-Version: 5.0.4
--- 
+-- Erstellungszeit: 30. Dez 2016 um 21:54
+-- Server Version: 5.5.53
+-- PHP-Version: 5.4.45-0+deb7u6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
 -- Datenbank: `wetter_sdr`
--- 
+--
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Tabellenstruktur für Tabelle `raw`
--- 
+--
 
-CREATE TABLE `raw` (
-  `idx` datetime NOT NULL default '0000-00-00 00:00:00',
-  `hum_out` decimal(3,0) default NULL,
-  `temp_out` decimal(4,1) default NULL,
-  `wind_ave` decimal(5,1) default NULL,
-  `wind_gust` decimal(5,1) default NULL,
-  `wind_dir` decimal(3,0) default NULL,
-  `rain_count` decimal(6,1) default NULL,
-  PRIMARY KEY  (`idx`)
+CREATE TABLE IF NOT EXISTS `raw` (
+  `idx` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `hum_out` decimal(3,0) DEFAULT NULL,
+  `temp_out` decimal(4,1) DEFAULT NULL,
+  `wind_ave` decimal(5,1) DEFAULT NULL,
+  `wind_gust` decimal(5,1) DEFAULT NULL,
+  `wind_dir` decimal(3,0) DEFAULT NULL,
+  `rain_count` decimal(6,1) DEFAULT NULL,
+  `lo_batt` int(2) DEFAULT NULL,
+  PRIMARY KEY (`idx`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- 
--- Daten für Tabelle `raw`
--- 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
